0ad00bdf20eced576ebcddff78964db42d467812
infinith4@gmail.com <infinith4@gmail.com>
2016-07-04 22:50:22 +0900

    modified model

--- Models/TodoItem.cs
+++ Models/TodoItem.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+
 namespace EmptyWebApplication.Models
 {
     public class TodoItem
@@ -5,5 +9,54 @@ namespace EmptyWebApplication.Models
         public string Key { get; set; }
         public string Name { get; set; }
         public bool IsComplete { get; set; }
+    }
+    public interface ITodoRepository
+    {
+        void Add(TodoItem item);
+        IEnumerable<TodoItem> GetAll();
+        TodoItem Find(string key);
+        TodoItem Remove(string key);
+        void Update(TodoItem item);
+    }
+    public class TodoRepository : ITodoRepository
+    {
+        private static ConcurrentDictionary<string, TodoItem> _todos = 
+              new ConcurrentDictionary<string, TodoItem>();
+
+        public TodoRepository()
+        {
+            Add(new TodoItem { Name = "Item1" });
+        }
+
+        public IEnumerable<TodoItem> GetAll()
+        {
+            return _todos.Values;
+        }
+
+        public void Add(TodoItem item)
+        {
+            item.Key = Guid.NewGuid().ToString();
+            _todos[item.Key] = item;
+        }
+
+        public TodoItem Find(string key)
+        {
+            TodoItem item;
+            _todos.TryGetValue(key, out item);
+            return item;
+        }
+
+        public TodoItem Remove(string key)
+        {
+            TodoItem item;
+            _todos.TryGetValue(key, out item);
+            _todos.TryRemove(key, out item);
+            return item;
+        }
+
+        public void Update(TodoItem item)
+        {
+            _todos[item.Key] = item;
+        }
     }
 }
